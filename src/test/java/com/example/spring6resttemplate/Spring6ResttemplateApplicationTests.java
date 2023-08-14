5fa80bf7eaa59bd2030d1ddc717e5ae568f6d646
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-08-14 14:42:38 +0330

    Get list as string close #2

--- src/test/java/com/example/spring6resttemplate/Spring6ResttemplateApplicationTests.java
+++ src/test/java/com/example/spring6resttemplate/Spring6ResttemplateApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.spring6resttemplate;
 
+import com.example.spring6resttemplate.client.BeerClient;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Spring6ResttemplateApplicationTests {
 
+    @Autowired
+    BeerClient beerClient;
+
     @Test
     void contextLoads() {
+
+        beerClient.listBeers();
     }
 
 }
