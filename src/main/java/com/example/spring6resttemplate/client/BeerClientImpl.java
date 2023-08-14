d1ea32b79f2e8e9f0424c67527caf92d597c82c3
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2023-08-14 15:31:22 +0330

    URL parameters close #11

--- src/main/java/com/example/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/com/example/spring6resttemplate/client/BeerClientImpl.java
@@ -13,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Map;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Service
@@ -20,6 +21,13 @@ public class BeerClientImpl implements BeerClient {
     private final RestTemplateBuilder restTemplateBuilder;
 
     private static final String GET_BEER_PATH = "/api/v1/beer";
+    private static final String GET_BEER_BY_ID_PATH = "/api/v1/beer/{beerId}";
+
+    @Override
+    public BeerDTO getBeerById(UUID beerId) {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        return restTemplate.getForObject(GET_BEER_BY_ID_PATH, BeerDTO.class, beerId);
+    }
     @Override
     public Page<BeerDTO> listBeers() {
         return this.listBeers(null, null, null, null, null);
--- src/test/java/com/example/spring6resttemplate/Spring6ResttemplateApplicationTests.java
+++ src/test/java/com/example/spring6resttemplate/Spring6ResttemplateApplicationTests.java
@@ -1,15 +1,31 @@
 package com.example.spring6resttemplate;
 
 import com.example.spring6resttemplate.client.BeerClient;
+import com.example.spring6resttemplate.model.BeerDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class Spring6ResttemplateApplicationTests {
 
     @Autowired
     BeerClient beerClient;
+
+    @Test
+    void getBeerById() {
+
+        Page<BeerDTO> beerDTOS = beerClient.listBeers();
+
+        BeerDTO dto = beerDTOS.getContent().get(0);
+
+        BeerDTO byId = beerClient.getBeerById(dto.getId());
+
+        assertNotNull(byId);
+    }
 
     @Test
     void listBeersNoBeerName() {
